859decbfe8327a2dca00c84b2c5fc61d344ef72c
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2026-05-17 15:42:41 +0700

    chore: pin nvim-treesitter to main branch (parser-only)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -56,7 +56,7 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/mfussenegger/nvim-lint" },
 	--------------------- COMPLETION ---------------------
