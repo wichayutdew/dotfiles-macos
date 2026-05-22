b760ff2ff807b0fbe80a4636b8236f05289ee462
Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>
2026-05-23 00:47:08 +0700

    fix(scala): move setup_dap inside FileType callback, remove unused dap require

--- .config/nvim/lua/plugins/scala.lua
+++ .config/nvim/lua/plugins/scala.lua
@@ -1,26 +1,25 @@
 -- Scala LSP (metals) + DAP Configuration
 return function()
-  local metals = require("metals")
-  local dap = require("dap")
+	local metals = require("metals")
 
-  ---------------------- Metals Config ---------------------
-  local metals_config = metals.bare_config()
+	---------------------- Metals Config ---------------------
+	local metals_config = metals.bare_config()
 
-  metals_config.capabilities = vim.lsp.protocol.make_client_capabilities()
+	metals_config.capabilities = vim.lsp.protocol.make_client_capabilities()
 
-  metals_config.settings = {
-    showImplicitArguments = true,
-    showInferredType = true,
-  }
+	metals_config.settings = {
+		showImplicitArguments = true,
+		showInferredType = true,
+	}
 
-  ---------------------- DAP Setup ---------------------
-  metals.setup_dap()
-
-  ---------------------- Attach on FileType ---------------------
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "scala", "sbt" },
-    callback = function()
-      metals.initialize_or_attach(metals_config)
-    end,
-  })
+	---------------------- Attach on FileType ---------------------
+	vim.api.nvim_create_autocmd("FileType", {
+		group = vim.api.nvim_create_augroup("metals", { clear = true }),
+		pattern = { "scala", "sbt" },
+		desc = "Attach metals LSP",
+		callback = function()
+			metals.initialize_or_attach(metals_config)
+			metals.setup_dap()
+		end,
+	})
 end
